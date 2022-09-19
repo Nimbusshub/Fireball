1f87a0eea545ea704fc0551d511ed1ea0caa8701
Nimah <busayonimota1603@gmail.com>
2022-09-19 15:19:06 +0100

    [Update]: Add listener usage prompt

--- models/scanUsage.py
+++ models/scanUsage.py
@@ -3,3 +3,8 @@
 def usageScan():
     print("scan {target specification} [-p port number(s) or range]\n\
         run 'man fireball' for help")
+
+
+def usageListener():
+    print("listen {}......\n\
+        run 'man fireball' for help")
